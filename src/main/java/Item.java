7f0865f7fb40d359f2990b58783d8be605355522
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-02-10 00:55:53 +0000

    Add files via upload
    Added files

---
+++ src/main/java/Item.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class Item { //class created to easily store an item's data
+    int quantity;
+    double price;
+    String name;
+}
